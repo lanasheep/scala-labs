16c9b9c7d2c2d7d2239df807f39a0e91e3bba586
Ruslana Tsehanovskaya <lanasheep@yandex.ru>
2020-06-14 00:17:41 +0300

    lab3 done

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -1,20 +1,38 @@
 package bot
 
-import cats.instances.future._
-import cats.syntax.functor._
 import com.bot4s.telegram.api.RequestHandler
-import com.bot4s.telegram.api.declarative.Commands
 import com.bot4s.telegram.clients.{FutureSttpClient}
 import com.bot4s.telegram.future.{Polling, TelegramBot}
-import com.softwaremill.sttp.SttpBackendOptions
 import com.softwaremill.sttp.okhttp.OkHttpFutureBackend
+import com.softwaremill.sttp.json4s.asJson
 import scala.collection.mutable.Set
 import scala.collection.mutable.Map
 import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
+import cats.instances.future._
+import cats.syntax.functor._
+import com.softwaremill.sttp._
+import com.bot4s.telegram.future.Polling
+import com.bot4s.telegram.api.declarative.Commands
 
-class Bot(override val client: RequestHandler[Future]) extends TelegramBot
+case class Response(data: List[Data])
+case class Data(link: String)
+
+class Service(implicit val backend: SttpBackend[Future, Nothing], implicit val ec: ExecutionContext) {
+  private implicit val serialization = org.json4s.native.Serialization
+
+  val request: RequestT[Id, Response, Nothing] = sttp
+    .header("Authorization", "Client-ID 653027b508dec6b")
+    .get(uri"https://api.imgur.com/3/gallery/search?q=cats")
+    .response(asJson[Response])
+
+  def getRandomCat() = backend.send(request).map { response =>
+    scala.util.Random.shuffle(response.unsafeBody.data).head.link
+  }
+}
+
+class Bot(override val client: RequestHandler[Future], val service: Service) extends TelegramBot
   with Polling
   with Commands[Future] {
   val users: Set[Int] = Set[Int]()
@@ -64,6 +82,9 @@ class Bot(override val client: RequestHandler[Future]) extends TelegramBot
     }
   }
 
+  onCommand("/cat") { implicit msg =>
+    service.getRandomCat.flatMap(link => reply(link)).void
+  }
 }
 
 object Bot {
@@ -73,8 +94,9 @@ object Bot {
       SttpBackendOptions.Default.socksProxy("ps8yglk.ddns.net", 11999)
     )
 
-    val token = ""
-    val bot = new Bot(new FutureSttpClient(token))
+    val token = "750606265:AAHK36Mu8tKZwwIOVweydES81XheJPG-cx8"
+    val service = new Service()
+    val bot = new Bot(new FutureSttpClient(token), service)
     Await.result(bot.run(), Duration.Inf)
   }
 }
